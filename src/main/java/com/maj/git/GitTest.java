f12a4334f4cc8e4389a216642dc863098f6d213d
majing1993 <1425262046@qq.com>
2021-11-07 13:47:53 +0800

    first commit

---
+++ src/main/java/com/maj/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.maj.git;
+
+/**
+ * @author maj
+ * @date 2021/11/7
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git...");
+    }
+}
